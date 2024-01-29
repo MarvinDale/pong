f92ccd2d2bbaf29707fa59705e8230acd6728ae2
Marvin Dale <marvinfrancisdale@gmail.com>
2024-01-29 22:15:35 +0000

    refactor Vector2d

--- Vector2d.cpp
+++ Vector2d.cpp
@@ -2,16 +2,12 @@ struct Vector2d {
     float x;
     float y;
 
+    Vector2d() : x(0), y(0) {}
+    Vector2d(float x, float y) : x(x), y(y) {}
+
     static Vector2d up()   { return Vector2d(0, -1); }
     static Vector2d down() { return Vector2d(0,  1); }
     static Vector2d zero() { return Vector2d(0,  0); }
-
-    Vector2d() : x(0), y(0) {}
-
-    Vector2d(float x, float y) {
-        this->x = x;
-        this->y = y;
-    }
 
     Vector2d operator+(float value) {
         Vector2d result;
